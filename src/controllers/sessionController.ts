b3dcf31ad5e852bb843da368faaa740d0caaf369
Steve <sntakirutimana72@gmail.com>
2023-07-06 21:47:45 +0200

    Move & rename SessionOAuth to SessionController

---
+++ src/controllers/sessionController.ts
@@ -0,0 +1,64 @@
+import { AxiosResponse } from "axios"
+import { ApplicationController, Resolve } from './applicationController'
+import { Axios } from '../helpers/requests'
+import { SessionStore } from '../helpers/store'
+
+type CredentialsProps = Pick<NewUser, 'email' | 'password'>
+
+export class SessionController extends ApplicationController {
+  protected static onSuccess(resp: AxiosResponse, resolve: Resolve<CurrentUser>) {
+    const { data: { user }, headers: { authorization } } = resp
+    SessionStore.persist(authorization)
+    resolve(user)
+  }
+
+  static login(creds: CredentialsProps) {
+    return new Promise<CurrentUser>((resolve, reject) => {
+      Axios
+        .post(process.env.REACT_APP_BLUECH_RB_API_LOGIN!, { user: creds })
+        .then((resp) => { this.onSuccess(resp, resolve) })
+        .catch(exc => { this.reject(exc, reject) })
+    })
+  }
+
+  static logout() {
+    return new Promise<void>((resolve) => {
+      Axios
+        .delete(process.env.REACT_APP_BLUECH_RB_API_LOGOUT!, this.authorize())
+        .finally(() => {
+          SessionStore.destroy()
+          resolve()
+        })
+    })
+  }
+
+  static register(requirements: NewUser) {
+    return new Promise<CurrentUser>((resolve, reject) => {
+      Axios
+        .post(process.env.REACT_APP_BLUECH_RB_API_REGISTER!, { user: requirements })
+        .then((resp) => { this.onSuccess(resp, resolve) })
+        .catch(exc => { this.reject(exc, reject) })
+    })
+  }
+
+  static signedUser() {
+    return new Promise<CurrentUser>((resolve, reject) => {
+      Axios
+        .get(process.env.REACT_APP_BLUECH_RB_API_SESSION_USER!, this.authorize())
+        .then((resp) => { this.onSuccess(resp, resolve) })
+        .catch(() => { reject() })
+    })
+  }
+
+  static refresh() {
+    return new Promise<void>((resolve, reject) => {
+      Axios
+        .head(process.env.REACT_APP_BLUECH_RB_API_REFRESH_SESSION!, this.authorize())
+        .then(({ headers: { authorization } }) => {
+          SessionStore.persist(authorization)
+          resolve()
+        })
+        .catch(() => { reject() })
+    })
+  }
+}
