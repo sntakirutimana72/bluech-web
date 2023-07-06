8e8ae13b61b3f334f590d967a8e02ab17505297c
Steve <sntakirutimana72@gmail.com>
2023-07-06 21:45:13 +0200

    Add an ApplicationController as a base for the api

---
+++ src/controllers/applicationController.ts
@@ -0,0 +1,25 @@
+import { isAxiosError, AxiosError } from "axios"
+import { SessionStore } from "../helpers/store"
+
+export type Resolve<T> = (param: PromiseLike<T> | T) => void
+export type Reject = (reason?: any) => void
+type ApiError = { error: string }
+
+
+export class ApplicationController {
+  protected static authorize() {
+    return {
+      headers: {
+        Authorization: SessionStore.fetch()!
+      }
+    }
+  }
+
+  protected static reject(error: Error | AxiosError<ApiError>, reject: Reject) {
+    if (isAxiosError(error)) {
+      reject(error.response?.data.error)
+    } else {
+      reject(error.message)
+    }
+  }
+}
