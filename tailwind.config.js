82c265f51b103ad26659213f2819dc3ea3f54c09
Steve <sntakirutimana72@gmail.com>
2023-07-10 23:01:44 +0200

    Add base colors extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueish: '#7182ff',
+        darkish: '#1f1f1f',
+        'darkish-2x': '#2f2f2f',
+        reddish: '#ee4f51',
+      },
+    },
   },
   plugins: [],
 }
