f5945f86a6a4b7d1516e846ffe8e5e6cbe87e6fa
Steve <sntakirutimana72@gmail.com>
2023-07-16 21:01:35 +0200

    Setup axios application instance

---
+++ src/helpers/requests/axios.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const Axios = axios.create({
+  baseURL: process.env.REACT_APP_BLUECH_RB_API_BASE_URL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default Axios
