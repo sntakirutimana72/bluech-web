1077e6f9abd35fedecf5ca4b33f95d0d8cf5fef5
Steve <sntakirutimana72@gmail.com>
2023-07-16 21:09:46 +0200

    Configure tailwindcss resources

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,60 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        blueish: '#7182ff',
+        blueish: '#a065ff',
         darkish: '#1f1f1f',
-        'darkish-2x': '#2f2f2f',
+        'darkish-2x': '#4f4f4f',
+        'darkish-2.5x': '#6f6f6f',
+        'darkish-3x': '#9f9f9f',
         reddish: '#ee4f51',
+        light: '#f5f6fa',
+      },
+      animation: {
+        toggled: 'toggled 350ms ease-in-out',
+        'load-ball-1': 'load-bounce 1s alternate infinite ease-in-out, load-flash 3s alternate infinite ease-in-out',
+        'load-ball-2': 'load-bounce 1s .333s alternate infinite ease-in-out, load-flash 3s 1s alternate infinite ease-in-out',
+        'load-ball-3': 'load-bounce 1s .666s alternate infinite ease-in-out, load-flash 3s 2s alternate infinite ease-in-out',
+      },
+      keyframes: {
+        toggled: {
+          '0%': {
+            opacity: 0,
+            display: 'none',
+            transform: 'translateX(100%)',
+          },
+          '1%': {
+            opacity: 1,
+            display: 'block',
+          },
+          '100%': {
+            transform: 'translateX(0)',
+          }
+        },
+        'load-bounce': {
+          from: {
+            position: 'relative',
+            top: '-10%',
+          },
+          to: {
+            position: 'relative',
+            top: '10%',
+          }
+        },
+        'load-flash': {
+          from: {
+            background: '#f5f6fa',
+          },
+          to: {
+            background: '#999',
+          }
+        },
+      },
+      transitionProperty: {
+        custom: 'display, width, height, right, top, bottom, left, transform',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
