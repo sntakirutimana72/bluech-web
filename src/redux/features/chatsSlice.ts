f9dc91b9f75c7d6b11c9436e5bce063ab19e1cd5
Steve <sntakirutimana72@gmail.com>
2023-07-19 19:16:49 +0200

    Remediate actions

--- src/redux/features/chatsSlice.ts
+++ src/redux/features/chatsSlice.ts
@@ -1,34 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit';
-import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
 
 type ChatsState = {
-  typings: {
-    [key: AlphaNumeric]: boolean
-  }
-  messages: {
-    [key: AlphaNumeric]: CableMessage[]
-  }
+  typings: { [key: AlphaNumeric]: boolean }
+  messages: { [key: AlphaNumeric]: CableMessage[] }
 }
 
 const initialState: ChatsState = {
   typings: {},
   messages: {},
-};
+}
+
 const slicer = createSlice({
   name: 'channels/chats',
   initialState,
   reducers: {
     userTyping(state, action: PayloadAction<AlphaNumeric>) {
-      state.typings[action.payload] = true;
+      state.typings[action.payload] = true
     },
     typingExpired(state, action: PayloadAction<AlphaNumeric>) {
-      delete state.typings[action.payload];
+      state.typings = Object.fromEntries(
+        Object.entries(state.typings).filter(([id]) => id !== action.payload),
+      )
     },
     mapMessage(state, action: PayloadAction<CableMessage>) {
-      state.messages[action.payload.author.id].push(action.payload);
+      const { payload } = action
+      const { author: { id } } = payload
+      const { messages } = state
+
+      if (!messages[id]) {
+        messages[id] = []
+      }
+      messages[id].push(payload)
     },
   },
 });
 
 export const { userTyping, typingExpired, mapMessage } = slicer.actions;
-export default slicer.reducer;
+export default slicer.reducer
