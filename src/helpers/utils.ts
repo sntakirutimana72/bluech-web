9aff3d75ed82fd3c16379937334fe1e7372705de
Steve <sntakirutimana72@gmail.com>
2023-08-22 15:29:07 +0200

    Add no effect empty function for dull observation

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,14 +1,7 @@
-/**
- * Returns FALSE if the given parameter is undefined, TRUE otherwise.
- * @param obj
- *
- */
 export const isInit = (obj: any) => obj !== undefined
 
-/**
- * Returns TRUE if the given parameter is null, FALSE otherwise
- * @param obj
- */
 export const isNil = (obj: any) => obj === null
 
 export const now = () => new Date()
+
+export const nilFunc = () => {}
