3fe1d08060c192b5920dd3f49c0fc0d1dc5482ab
Steve <sntakirutimana72@gmail.com>
2023-08-30 13:28:59 +0200

    Test :read event emission

--- src/specs/channels/chats.spec.ts
+++ src/specs/channels/chats.spec.ts
@@ -1,11 +1,7 @@
 import { TestCable } from '@anycable/core/testing'
 import { ChatsChannel } from '../../channels'
 import Generic from '../support/mocks/generic'
-import type { TypingMessage, ChatMessage } from '../../channels'
-
-afterAll(() => {
-  Generic.clear()
-})
+import type { TypingMessage, ChatMessage, AsSeenMessage } from '../../channels'
 
 describe('ChatsChannel', () => {
   let channel: ChatsChannel
@@ -19,6 +15,10 @@ describe('ChatsChannel', () => {
     channel = cable.subscribe(new ChatsChannel())
   })
 
+  afterAll(() => {
+    Generic.clear()
+  })
+
   test('perform typing action', async () => {
     const typingSignal = { channelId: id, status: true }
     await channel.typing(typingSignal)
@@ -28,14 +28,19 @@ describe('ChatsChannel', () => {
   test('fires events on receipt', () => {
     const onTyping = jest.fn()
     const onMessage = jest.fn()
+    const onSeen = jest.fn()
+    const asSeenMessage: AsSeenMessage = { type: 'read', readerId: 2, ids: ['2'] }
     const typingMessage: TypingMessage = { type: 'typing', author: partner, status: false }
     const chatMessage: ChatMessage = { type: 'message', message: Generic.cableMessage(undefined, id) }
 
     channel.on('typing', onTyping)
     channel.on('message', onMessage)
+    channel.on('read', onSeen)
+    channel.receive(asSeenMessage)
     channel.receive(chatMessage)
     channel.receive(typingMessage)
 
+    expect(onSeen).toHaveBeenCalled()
     expect(onTyping).toHaveBeenCalled()
     expect(onMessage).toHaveBeenCalled()
   })
