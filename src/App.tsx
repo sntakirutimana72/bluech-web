da214321d540b39cc0683836ca02868a26f1b252
Steve <sntakirutimana72@gmail.com>
2023-10-16 17:07:38 +0200

    Integrate static app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { useSession } from './hooks'
-import { RedirectedRoute, PrivateRoute } from './middlewares'
-import { Login, Logout, Register } from './components/Session'
-import GetStarted from './components/GetStarted'
-import Dashboard from './components/Dashboard'
-import Inbox from './components/Inbox'
-import People from './components/People'
-import ChatRoom from './components/ChatRoom'
-import NotFound from './components/NotFound'
+import { useSession } from '@/hooks'
+import { PublicRoute, PrivateRoute } from '@/middlewares'
+import { Login, Logout, Register } from 'src/components/session'
+import routes, { urls } from '@/config/routes'
+import GetStarted from '@/components/GetStarted'
+import Dashboard from '@/components/Dashboard'
+import Inbox from '@/components/Inbox'
+import People from '@/components/People'
+import Chatroom from '@/components/Chatroom'
+import NotFound from '@/components/NotFound'
 
 const App = () => {
   const { authenticated, logout, login } = useSession()
@@ -15,22 +16,22 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<RedirectedRoute authenticated={authenticated} redirectTo="/dashboard" />}>
-          <Route path="" element={<GetStarted />} />
-          <Route path="users/register" element={<Register login={login} />} />
-          <Route path="users/login" element={<Login login={login} />} />
+        <Route element={<PublicRoute authenticated={authenticated} redirectTo={urls.HOME} />}>
+          <Route path={routes.LANDING} element={<GetStarted />} />
+          <Route path={routes.REGISTER} element={<Register login={login} />} />
+          <Route path={routes.LOGIN} element={<Login login={login} />} />
         </Route>
 
-        <Route element={<PrivateRoute authenticated={authenticated} redirectTo="/" />}>
-          <Route path="dashboard" element={<Dashboard />}>
+        <Route element={<PrivateRoute authenticated={authenticated} redirectTo={urls.LANDING} />}>
+          <Route path={routes.home.INDEX} element={<Dashboard />}>
             <Route index element={<Inbox />} />
-            <Route path="people" element={<People />} />
-            <Route path="chats/:channelId" element={<ChatRoom />} />
+            <Route path={routes.home.PEOPLE} element={<People />} />
+            <Route path={routes.home.CHATROOM} element={<Chatroom />} />
           </Route>
-          <Route path="users/logout" element={<Logout logout={logout} />} />
+          <Route path={routes.LOGOUT} element={<Logout logout={logout} />} />
         </Route>
 
-        <Route path="*" element={<NotFound />} />
+        <Route path={routes.ANY} element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
