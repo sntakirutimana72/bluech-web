f3a5fd13d1295761c618299b37c4987da0bf5932
Steve <sntakirutimana72@gmail.com>
2023-10-18 22:08:33 +0200

    Style Register page

--- src/components/session/Register/index.tsx
+++ src/components/session/Register/index.tsx
@@ -2,18 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { urls } from '@/config/routes'
 import type { SessionContext } from '@/providers'
+import AppLogo from '@/assets/logos/logo.png'
+import { ButtonWithImg } from '@/components/elements/Buttons'
 import Form from './Form'
 
 type Props = Pick<SessionContext, 'login'>
 
 const Register = ({ login }: Props) => (
   <div className="session-pg register">
-    <div className="session-logo">
-      <img src="" alt="logo" />
-    </div>
+    <nav>
+      <ButtonWithImg className="text-gradient" text="bluech" src={AppLogo} sticker="Logo" />
+    </nav>
     <Form className="session-form register-form" login={login} />
     <Link to={urls.LOGIN} className="already-have-acc">
-      <i>Already have an account?</i>
+      Already have an account?
     </Link>
   </div>
 )
