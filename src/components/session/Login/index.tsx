8585d4e98dc91e8e81fa07872162e8d8dad26e5c
Steve <sntakirutimana72@gmail.com>
2023-10-19 13:35:17 +0200

    Replace <nav /> with <HeadBar />

--- src/components/session/Login/index.tsx
+++ src/components/session/Login/index.tsx
@@ -3,16 +3,21 @@ import { Link } from 'react-router-dom'
 import { urls } from '@/config/routes'
 import type { SessionContext } from '@/providers'
 import AppLogo from '@/assets/logos/logo.png'
-import { ButtonWithImg } from '@/components/elements/Buttons'
+import { HeadBar } from '@/components/navs'
 import Form from './Form'
 
 type Props = Pick<SessionContext, 'login'>
 
 const Login = ({ login }: Props) => (
   <div className="session-pg login">
-    <nav>
-      <ButtonWithImg className="text-gradient" text="bluech" src={AppLogo} sticker="Logo" />
-    </nav>
+    <HeadBar
+      className="text-gradient"
+      others={{
+        text: process.env.REACT_APP_NAME!,
+        src: AppLogo,
+        sticker: 'Logo',
+      }}
+    />
     <Form className="session-form login-form" login={login} />
     <Link to={urls.LANDING} className="forgot-password">Forgot password?</Link>
     <div className="session-otherwise">
--- src/components/session/Register/index.tsx
+++ src/components/session/Register/index.tsx
@@ -3,16 +3,21 @@ import { Link } from 'react-router-dom'
 import { urls } from '@/config/routes'
 import type { SessionContext } from '@/providers'
 import AppLogo from '@/assets/logos/logo.png'
-import { ButtonWithImg } from '@/components/elements/Buttons'
+import { HeadBar } from '@/components/navs'
 import Form from './Form'
 
 type Props = Pick<SessionContext, 'login'>
 
 const Register = ({ login }: Props) => (
   <div className="session-pg register">
-    <nav>
-      <ButtonWithImg className="text-gradient" text="bluech" src={AppLogo} sticker="Logo" />
-    </nav>
+    <HeadBar
+      className="text-gradient"
+      others={{
+        text: process.env.REACT_APP_NAME!,
+        src: AppLogo,
+        sticker: 'Logo',
+      }}
+    />
     <Form className="session-form register-form" login={login} />
     <Link to={urls.LOGIN} className="already-have-acc">
       Already have an account?
