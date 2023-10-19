552d88dbae51b58454b4f6ee9c9b23e5085fdb71
Steve <sntakirutimana72@gmail.com>
2023-10-19 13:38:57 +0200

    Add new routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,10 +1,12 @@
 const routes = {
   ANY: '*',
+  ACCOUNT: 'users/account',
   LOGOUT: 'users/logout',
   home: {
     INDEX: 'dashboard',
     PEOPLE: 'people',
     CHATROOM: 'chats/:channelId',
+    SEARCH: 'search',
   },
   LOGIN: 'users/login',
   REGISTER: 'users/register',
@@ -13,11 +15,13 @@ const routes = {
 
 export const urls = {
   LOGOUT: `/${routes.LOGOUT}`,
+  ACCOUNT: `/${routes.ACCOUNT}`,
   HOME: `/${routes.home.INDEX}`,
   PEOPLE: `/${routes.home.INDEX}/${routes.home.PEOPLE}`,
   chatroom(channelId: string) {
     return `${this.HOME}/chats/${channelId}`
   },
+  SEARCH: `/${routes.home.INDEX}/${routes.home.SEARCH}`,
   LOGIN: `/${routes.LOGIN}`,
   REGISTER: `/${routes.REGISTER}`,
   LANDING: `/${routes.LANDING}`,
